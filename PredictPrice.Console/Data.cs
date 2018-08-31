db196570807998082e678f8a61085f91443cfb2d
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2018-08-31 08:19:16 +0100

    Basic House Pricing

---
+++ PredictPrice.Console/Data.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+
+namespace PredictPrice
+{
+    public class HousePrice
+    {
+        [Column("0")]
+        public string TID;
+
+        [Column("1")]
+        public float Price;
+
+        [Column("2")]
+        public string DateOfTransfer;
+
+        [Column("3")]
+        public string PostCode;
+
+        [Column("4")]
+        public string PropertyType;
+
+        [Column("5")]
+        public string OldNew;
+
+        [Column("6")]
+        public string Duration;
+
+        [Column("7")]
+        public string PAON;
+        [Column("8")]
+        public string SAON;
+        [Column("9")]
+        public string Street;
+        [Column("10")]
+        public string Locality;
+        [Column("11")]
+        public string City;
+        [Column("12")]
+        public string District;
+        [Column("13")]
+        public string RecordStatus;
+    }
+
+    public class HousePricePredicted
+    {
+        [ColumnName("Score")]
+        public float PricePaid;
+    }
+}
---
+++ PredictPrice.Console/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using Newtonsoft.Json;
+using PredictPrice;
+
+namespace PredictPrice.ConsoleProject
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PredictionModel<HousePrice, HousePricePredicted> model = Train();
+            Evaluate(model);
+            Console.ReadLine();
+        }
+        private static void Evaluate(PredictionModel<HousePrice, HousePricePredicted> model)
+        {
+            var _testdatapath = @"pp-monthly-update-new-version.csv";
+            var testData = new TextLoader(_testdatapath).CreateFrom<HousePrice>(useHeader: false, separator: ',');
+            var evaluator = new RegressionEvaluator() { LabelColumn = "Price" };
+            RegressionMetrics metrics = evaluator.Evaluate(model, testData);
+            Console.WriteLine(JsonConvert.SerializeObject(metrics,Formatting.Indented));
+        }
+        public static PredictionModel<HousePrice, HousePricePredicted> Train()
+        {
+            string _datapath = @"pp-2018.csv";
+            var pipeline = new LearningPipeline();
+            var dropper = new ColumnDropper();
+
+            pipeline.Add(new TextLoader(_datapath).CreateFrom<HousePrice>(useHeader: false, separator: ','));
+            pipeline.Add(new ColumnDropper() { Column = new string[] { "DateOfTransfer", "Street", "Locality", "PAON", "SAON" } });
+            
+            pipeline.Add(new CategoricalOneHotVectorizer(
+                    "PostCode",
+                    "City",
+                    "District",
+                    "RecordStatus",
+                    "Duration",
+                    "PropertyType",
+                    "OldNew"));
+
+            pipeline.Add(new ColumnConcatenator(
+                    "Features",
+                    "PostCode",
+                    "City",
+                    "District",
+                    "RecordStatus",
+                    "Duration",
+                    "PropertyType",
+                    "OldNew"));
+
+            pipeline.Add(new FastTreeRegressor() { LabelColumn = "Price" });
+            return pipeline.Train<HousePrice, HousePricePredicted>();
+        }
+    }
+}
